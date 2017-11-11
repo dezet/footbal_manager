7aa52b4f124d82fc7380c24189d88756388683a1
Damian Kryger <damiankryger@gmail.com>
2017-11-11 01:01:20 +0100

    dodanie Axiosa i dodanie adnotacji CrossOrigin w PlayerControllerze

---
+++ web/src/components/Players.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="players">
+    <h1>Gracze</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Players',
+  created () {
+    axios.post(`http://localhost:8124/players`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        console.log(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+}
+</script>
