45684d1572cbef62d96e7d330d4ad9e474412e32
Damian Kryger <damiankryger@gmail.com>
2017-12-17 18:05:15 +0100

    Vue main page first layout

---
+++ web/src/components/MainMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav id="mainMenu" class="navbar navbar-inverse navbar-static-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menuCollapse" aria-expanded="false">
+          <span class="sr-only">Rozwiń menu</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Football manager</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="menuCollapse">
+        <ul class="nav navbar-nav">
+          <li>
+            <a href="#">Test 1</a>
+          </li>
+          <li>
+            <a href="#">Test 2</a>
+          </li>
+          <li>
+            <a href="#">Test 3</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'main-menu'
+  }
+</script>
+
+<style scoped>
+
+</style>
