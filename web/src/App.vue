4bd7627b77bbdd7dfc9cff0524dba87f645123cc
Damian Kryger <damiankryger@gmail.com>
2017-12-17 18:05:15 +0100

    Vue main page first layout

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header>
+      <main-menu/>
+    </header>
+
+    <div class="wrapper">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import MainMenu from '@/components/MainMenu'
+
+  export default {
+    name: 'app',
+    components: {
+      'main-menu': MainMenu
+    }
+  }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
 import Players from '@/components/Players'
 
 Vue.use(Router)
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
     },
     {
       path: '/players',
