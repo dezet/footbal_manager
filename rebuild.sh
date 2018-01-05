f55065ac7f016f9a1c304737d264e79f138498a2
domc1o <deezet@gmail.com>
2018-01-05 16:16:02 +0100

    git ignore, rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose down
-gradle build -x test #przy testach potrzeba bazy bo inaczej liquibase wypierdala wyjatkiem
+gradle build -x test #przy testach potrzeba bazy bo inaczej liquibase wypierdala wyjatkiem, pozniej dobrze by bylo jednak tu te testy puszczac XD
 cp ./build/libs/footbal_manager-0.0.1-SNAPSHOT.jar ./src/main/resources/docker/dist/
 docker-compose build
 docker-compose up
